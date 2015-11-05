11ef7422f11e3b9ac8c2013892fc30600c4c0c03
Robert Rozas Navarro <robert.rozas@abgrupp.com>
2015-11-05 18:08:34 -0300

    Pruebas con web api  y

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 (function() {
-  var app = angular.module('gemStore', ['store-directives']);
+  var app = angular.module('gemStore', ['ngResource','store-directives']);
 
-  app.controller('StoreController', ['$http', function($http){
+  app.controller('StoreController', ['$http','$resource', function($http,$resource){
     var store = this;
     store.products = [];
     $http.get('./store-products.json').success(function(data){
         store.products = data;
     });
+
+    $http.post('http://localhost:60525/vproyectos/Usuario/Login').success(function(data){
+        console.log(data);
+    });
+
+    var test = $resource('http://localhost:60525/vproyectos/Usuario/Login');
+    test.save(function(data){
+      console.log('Hola',data.respuesta);
+    });
+
   }]);
 
   app.controller('ReviewController', function() {
@@ -18,4 +28,5 @@
       this.review = {};
     };
   });
+
 })();
